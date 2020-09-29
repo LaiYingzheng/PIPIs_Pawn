220caf4825641cf1148a2dc1b413add5d377e4d8
yingz <w.yingzheng.lai@gmail.com>
2020-09-29 18:44:06 -0400

    First update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import EasyPreview from "easy-preview";
+import VueHtml2Canvas from 'vue-html2canvas';
+import HOMEPAGE from "@/components/HOME.vue";
+import VIDEOTAG from "@/components/Project.vue";
+import Pre from "@/components/preview.vue";
 
+Vue.use(VueHtml2Canvas);
+Vue.use(VueRouter);
+Vue.use(EasyPreview);
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+    routes: [
+        {path: '/', name:'home', component: HOMEPAGE},
+        {path: '/audiotag', name: 'project',component:VIDEOTAG},
+        {path: '/pre', name: 'project',component:Pre},
+    ]
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
